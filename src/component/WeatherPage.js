71a81012af9cb227c9dbfdeefaf5eedcb6017945
Andrew Garcia <andrewmanuelgarcia@gmail.com>
2021-01-26 20:01:29 -0500

    api hooked up and working, having issue formatting a week day from api (doesnt exist?) & getting continue button on log-in page to work. otherwise looking good

--- src/component/WeatherPage.js
+++ src/component/WeatherPage.js
@@ -1,5 +1,6 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { Context } from '../component/Context'
+import axios from 'axios'
 import './WeatherPage.css'
 import Card from './Card'
 
@@ -33,10 +34,61 @@ const testData = [
 ]
 
 
-
 const WeatherPage = (props) => {
     const {name, setName} = useContext(Context);
     const {location, setLocation} = useContext(Context);
+    var url = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${location}&appid=85f48bfed6f16f0b4ac5840f56199b17`;
+
+    const [weather, setWeather] = useState({
+        temp1: "", //We will use 6,12,18,24,32
+        temp2: "",
+        temp3: "",
+        temp4: "",
+        temp5: "",
+        date1: "",
+        date2: "",
+        date3: "",
+        date4: "",
+        date5: "",
+        day1: "",
+        day2: "",
+        day3: "",
+        day4: "",
+        day5: ""
+      });
+    
+      useEffect(() => {
+        axios
+          .get(url)
+          .then((res) => {
+            console.log("Response: ", res.data);
+            setWeather({
+              // Day 1
+              temp1: res.data.list[6].main.temp,
+              date1: res.data.list[6].dt_txt,
+              day1: "Monday",
+              // Day 2
+              temp2: res.data.list[12].main.temp,
+              date2: res.data.list[12].dt_txt,
+              day2: "Tuesday",
+              // Day 3
+              temp3: res.data.list[18].main.temp,
+              date3: res.data.list[18].dt_txt,
+              day3: "Wednesday",
+              // Day 4
+              temp4: res.data.list[24].main.temp,
+              date4: res.data.list[24].dt_txt,
+              day4: "Thusday",
+              // Day 5
+              temp5: res.data.list[32].main.temp,
+              date5: res.data.list[32].dt_txt,
+              day5: "Friday",
+            });
+          })
+          .catch((err) => {
+            console.log("Error: ", err);
+          });
+      }, []);
 
     return(
         <div className="weather-page">
@@ -46,7 +98,7 @@ const WeatherPage = (props) => {
                     <h3 className="weather-forecast-text">Weather forecast: {location} for the next 5 days.</h3>
                 </div>
                 <div className="weather-boxes">
-                    <p className="boxes">{testData.map((banana) => {
+                    {/* <p className="boxes">{testData.map((banana) => {
                         return(
                         <Card 
                             temp={banana.temp}
@@ -54,7 +106,34 @@ const WeatherPage = (props) => {
                             date={banana.date}
                         />
                         )})}
-                    </p>
+                    </p> */}
+                <div className="boxes">
+                    <Card 
+                            temp={weather.temp1}
+                            day={weather.day1}
+                            date={weather.date1}
+                        />
+                    <Card 
+                            temp={weather.temp2}
+                            day={weather.day2}
+                            date={weather.date2}
+                        />
+                    <Card 
+                            temp={weather.temp3}
+                            day={weather.day3}
+                            date={weather.date3}
+                        />
+                    <Card 
+                            temp={weather.temp4}
+                            day={weather.day4}
+                            date={weather.date4}
+                        />
+                    <Card 
+                            temp={weather.temp5}
+                            day={weather.day5}
+                            date={weather.date5}
+                        />
+                </div>
                 </div>
             </div>
         </div>
